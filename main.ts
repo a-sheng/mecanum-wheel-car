686adb755436f981251359bc14f4250acef7d690
Jackie Huang <jackie.sheng@gmail.com>
2023-02-10 15:03:27 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-radio.onReceivedValue(function (name, value) {
-    if (name == "x") {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-        motorbit.MotorRunDual(motorbit.Motors.M1, 150, motorbit.Motors.M2, 150)
-        motorbit.MotorRunDual(motorbit.Motors.M3, 150, motorbit.Motors.M4, 150)
-    } else {
-        motorbit.motorbit_rus04(RgbUltrasonics.All, RgbColors.Blue, ColorEffect.Breathing)
-        motorbit.MotorStopAll()
-    }
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(3)
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "Bt") {
+        Bt = value
+    } else if (name == "Lx") {
+        Lx = value
+    } else if (name == "Ly") {
+        Ly = value
+    } else if (name == "Rx") {
+        Rx = value
+    } else if (name == "Ry") {
+        Ry = value
+    }
+    basic.showNumber(Bt)
+    serial.writeNumber(Lx)
+    serial.writeNumber(Ly)
+    serial.writeNumber(Rx)
+    serial.writeNumber(Ry)
+    serial.writeNumber(Bt)
+})
+let Ry = 0
+let Rx = 0
+let Ly = 0
+let Lx = 0
+let Bt = 0
 radio.setGroup(1)
+basic.forever(function () {
+	
+})
