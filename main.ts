4b58ac5e75629a805248353ddb3b9ef771c61e35
Jackie Huang <jackie.sheng@gmail.com>
2023-02-11 15:45:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,20 +8,22 @@ radio.onReceivedValue(function (name, value) {
     } else if (name == "Rx") {
         Rx = value
     } else if (name == "Ry") {
-        Ry = value
+        Ry = value * 1.1
     }
 })
+let denominator = 0
 let Ry = 0
 let Rx = 0
 let Ly = 0
 let Lx = 0
 let Bt = 0
-radio.setGroup(220)
+radio.setGroup(1)
 basic.forever(function () {
-    motorbit.MotorRunDual(motorbit.Motors.M3, Ly + Lx + Rx, motorbit.Motors.M4, Ly - Lx + Rx)
-    motorbit.MotorRunDual(motorbit.Motors.M1, Ly - Lx - Rx, motorbit.Motors.M2, Ly + Lx - Rx)
+    denominator = Math.max(Math.abs(Ly) + Math.abs(Lx) + Math.abs(Rx), 1)
+    motorbit.MotorRunDual(motorbit.Motors.M3, (Ly + Lx + Rx) / denominator, motorbit.Motors.M4, (Ly - Lx + Rx) / denominator)
+    motorbit.MotorRunDual(motorbit.Motors.M1, (Ly - Lx - Rx) / denominator, motorbit.Motors.M2, (Ly + Lx - Rx) / denominator)
     radio.sendNumber(motorbit.Ultrasonic_reading_distance())
-    if (Bt ** 2 == 1) {
+    if (Bt == 1) {
         music.playSoundEffect(music.createSoundEffect(
         WaveShape.Square,
         78,
@@ -31,6 +33,42 @@ basic.forever(function () {
         100,
         SoundExpressionEffect.None,
         InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.InBackground)
+    }
+    if (Bt == 2) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Noise,
+        500,
+        499,
+        255,
+        255,
+        750,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.InBackground)
+    }
+    if (Bt == 4) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sawtooth,
+        200,
+        3607,
+        255,
+        255,
+        100,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Logarithmic
+        ), SoundExpressionPlayMode.InBackground)
+    }
+    if (Bt == 8) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        2760,
+        4543,
+        255,
+        255,
+        100,
+        SoundExpressionEffect.Tremolo,
+        InterpolationCurve.Logarithmic
         ), SoundExpressionPlayMode.InBackground)
     }
     if (Ly == 0 && Lx == 0) {
